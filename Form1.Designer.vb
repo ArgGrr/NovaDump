321c41c1fdab9c515c23182aee77cf692ce47118
ArgGrr <arggrr@gmail.com>
2016-06-03 16:23:04 +1000

    Initial Upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtSampleRate = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtChannels = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtBits = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(539, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Drag and drop NovaStorm files here to dump out audio tracks"
+        '
+        'txtSampleRate
+        '
+        Me.txtSampleRate.Location = New System.Drawing.Point(37, 106)
+        Me.txtSampleRate.Name = "txtSampleRate"
+        Me.txtSampleRate.Size = New System.Drawing.Size(100, 29)
+        Me.txtSampleRate.TabIndex = 1
+        Me.txtSampleRate.Text = "7937"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(143, 110)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(165, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Hz - Sample Rate"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(143, 145)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(96, 25)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Channels"
+        '
+        'txtChannels
+        '
+        Me.txtChannels.Location = New System.Drawing.Point(37, 141)
+        Me.txtChannels.Name = "txtChannels"
+        Me.txtChannels.Size = New System.Drawing.Size(100, 29)
+        Me.txtChannels.TabIndex = 3
+        Me.txtChannels.Text = "2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(143, 180)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(149, 25)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Bits per Sample"
+        '
+        'txtBits
+        '
+        Me.txtBits.Location = New System.Drawing.Point(37, 176)
+        Me.txtBits.Name = "txtBits"
+        Me.txtBits.Size = New System.Drawing.Size(100, 29)
+        Me.txtBits.TabIndex = 5
+        Me.txtBits.Text = "8"
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(841, 228)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtBits)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtChannels)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtSampleRate)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtSampleRate As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtChannels As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtBits As System.Windows.Forms.TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,131 @@
+﻿Imports System.IO
+
+Public Enum ChunkTypes As UInt32
+    SHED = 1145391187
+    PCFM = 1296450384
+    SPAL = 1279348819
+    SAMP = 1347240275
+
+    FRAM = 1296126534
+    EOFL = 1279676229
+
+End Enum
+Public Class Form1
+
+    Private Sub ConvertFile(Filename As String)
+
+        Dim outfile As String
+
+        Dim chunkData() As Byte
+        Dim chunkLen As UInt32
+        Dim chunkType As ChunkTypes
+
+        Dim FileSize As UInteger = 0
+        Dim DataSize As UInteger = 0
+        Dim DataOffset As Long
+
+        Dim SampleRate As UInt32
+        Dim Channels As UInt16
+        Dim BitsPerChannel As UInt16
+
+        If Not UInt32.TryParse(txtSampleRate.Text, SampleRate) Then
+            SampleRate = 7937UI
+        End If
+        If Not UInt16.TryParse(txtChannels.Text, Channels) Then
+            Channels = 2US
+        End If
+        If Not UInt16.TryParse(txtBits.Text, BitsPerChannel) Then
+            BitsPerChannel = 8US
+        End If
+
+        outfile = Filename & ".wav" 'Path.ChangeExtension(filename, "wav")
+
+        If File.Exists(outfile) Then File.Delete(outfile)
+
+
+        Using bo As New BinaryWriter(New FileStream(outfile, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+            Using br As New BinaryReader(New FileStream(Filename, FileMode.Open, FileAccess.Read, FileShare.Read))
+
+                'Write header..?
+                bo.Write(1179011410UI)  'RIFF
+                bo.Write(0UI)           'File Size, come back at end.
+
+                bo.Write(1163280727UI)  'WAVE
+                bo.Write(544501094UI)   'fmt 
+
+                bo.Write(16UI)          'dwChunkSize
+                bo.Write(1US)           'wFormatTag
+                bo.Write(Channels)           'nChannels
+                bo.Write(SampleRate)        'nSamplesPerSec
+                bo.Write(CUInt(SampleRate * Channels))       'avgBytesperSec
+                bo.Write(2US)           'nBlockAlign
+                bo.Write(8US)           'wBitsPerSample
+
+                bo.Write(1635017060UI)  'data
+                DataOffset = bo.BaseStream.Position
+                bo.Write(0UI)
+
+                Do
+                    chunkType = CType(br.ReadUInt32, ChunkTypes)
+                    'Check type
+                    chunkLen = br.ReadUInt32
+                    DataSize += chunkLen
+
+                    chunkData = br.ReadBytes(chunkLen)
+
+
+                    Select Case chunkType
+                        Case ChunkTypes.SAMP
+                            bo.Write(chunkData)
+                    End Select
+                Loop While chunkType <> ChunkTypes.EOFL
+
+                FileSize = CUInt(bo.BaseStream.Length)
+
+                bo.Seek(4, SeekOrigin.Begin)
+                bo.Write(CUInt(FileSize - 8))
+
+                bo.Seek(40, SeekOrigin.Begin)
+                bo.Write(CUInt(DataSize))
+            End Using
+        End Using
+        Beep()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+        Dim filename As String
+
+        filename = txtSampleRate.Text
+
+        If Not File.Exists(filename) Then Exit Sub
+
+        ConvertFile(filename)
+    End Sub
+
+    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragDrop
+
+        Debug.Print(e.ToString)
+        Dim files() As String
+
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            files = CType(e.Data.GetData(DataFormats.FileDrop), String())
+
+            For Each tmpstring As String In files
+                If File.Exists(tmpstring) Then
+                    ConvertFile(tmpstring)
+                End If
+            Next
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles Me.DragEnter
+
+        e.Effect = DragDropEffects.All
+    End Sub
+
+    Private Sub Form1_DragOver(sender As Object, e As DragEventArgs) Handles Me.DragOver
+        e.Effect = DragDropEffects.All
+    End Sub
+End Class
